6e5521d5047c75085aa52bafee8025dd4f536aed
Brian Woodard <bwoodard@globalcashcard.com>
2015-11-19 09:16:11 -0800

    ini commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,40 +1,15 @@
-# == Class: fusionreactor
-#
-# Full description of class fusionreactor here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the function of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'fusionreactor':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
+# == Class: fusionreactor 
+# #
+# # Installs fusionreactor it on target hosts. Included in the ::security:: profile.
+# #
+# # === Authors
+# #
+# # Brian Woodard <bwoodard@globalcashcard.com>
+# #
+# # === Copyright
+# #
+# # Copyright 2014 Global Cash Card, unless otherwise noted.
+
 class fusionreactor {
   if $::frinstalled == false {
     file { '/tmp/FusionReactor_linux-x64_5_2_7.sh':
@@ -51,10 +26,13 @@ class fusionreactor {
       owner  => 'root',
       group  => 'wheel',
     }
-  exec { 'fr_install':
-    command   => 'bash FusionReactor_linux-x64_5_2_7.sh -q -varfile response.varfile',
-    require   => '/tmp/FusionReactor_linux-x64_5_2_7.sh',
-    creates   => '/opt/fusionreactor/',
-    logoutput => true,
+    exec { 'fr_install':
+      command   => 'bash FusionReactor_linux-x64_5_2_7.sh -q -varfile response.varfile',
+      require   => [ File[/tmp/FusionReactor_linux-x64_5_2_7.sh],
+                      File[/tmp/response.varfile]
+                      ]
+      creates   => '/opt/fusionreactor/',
+      logoutput => true,
+    }
   }
 }
